8d1ed45f6b04c4dfc41de03250d896fede39d759
ForSertee <feng_life@foxmail.com>
2025-04-08 19:55:19 +0800

    更新配置文件以调整侧边栏列数至3，修改跳过渲染的文件规则，并新增Google站点验证的生成脚本，同时删除了原有的验证HTML文件。

---
+++ scripts/google-verification.js
@@ -0,0 +1,6 @@
+hexo.extend.generator.register('google-verification', function(locals) {
+    return {
+      path: 'google3c717581e81205ca.html',
+      data: 'google-site-verification: google3c717581e81205ca.html'
+    };
+  });
