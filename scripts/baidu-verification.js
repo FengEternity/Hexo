68c412dbbd08ed9c78204406330a9962b991e272
ForSertee <feng_life@foxmail.com>
2025-04-08 20:38:38 +0800

    新增百度站点验证生成脚本，支持自动生成验证HTML文件。

---
+++ scripts/baidu-verification.js
@@ -0,0 +1,6 @@
+hexo.extend.generator.register('baidu-verification', function(locals) {
+    return {
+      path: 'baidu_verify_codeva-HcXpfotEws.html',
+      data: 'e6ef62d3a4c817527e92053674324ee7'
+    };
+  });
